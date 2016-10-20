b46ccbc527664919bd2b913e2904caf8881bbd33
Naveena Sivaprabu <naveenasivaprabu@gmail.com>
2016-10-20 17:57:06 +0530

    Controller Image Changed

--- 01/04-Controllers/002/script.js
+++ 01/04-Controllers/002/script.js
@@ -2,7 +2,7 @@ var mainController = function($scope) {
     var person = {
         name: "Sivaprabu",
         age: 33,
-        imageLink: "https://en.gravatar.com/userimage/24424863/0627e2ae09d54bee1cfd0a97106c8f23.jpeg"
+        imageLink: "https://assets.about.me/background/users/g/s/i/gsivaprabu_1445923080_37.jpg"
     }
     $scope.message = "Hi Pranav.."
     $scope.person = person;
