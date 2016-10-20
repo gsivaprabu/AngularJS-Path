3219eda8d3e337e096602eecafe5aa900f3734ee
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-10-20 10:47:05 +0530

    Revert "Readme Changes"
    This reverts commit a97cbe534bc57c11b33b0f80c8d762b1571470c4.

---
+++ 01/04-Controllers/002/script.js
@@ -0,0 +1,9 @@
+var mainController = function($scope) {
+    var person = {
+        name: "Sivaprabu",
+        age: 33,
+        imageLink: "https://en.gravatar.com/userimage/24424863/0627e2ae09d54bee1cfd0a97106c8f23.jpeg"
+    }
+    $scope.message = "Hi Pranav.."
+    $scope.person = person;
+}
