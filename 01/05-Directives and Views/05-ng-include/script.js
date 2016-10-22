3fafa66dda420dd88ed31f49d969ee3a822e0f21
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-10-22 23:09:24 +0530

    Services

---
+++ 01/05-Directives and Views/05-ng-include/script.js
@@ -0,0 +1,31 @@
+(function() {
+    var app = angular.module("githubViewer", []);
+    var MainController = function($scope, $http) {
+        var onUserComplete = function(response) {
+            $scope.error = "";
+            $scope.user = response.data;
+            if ($scope.user.company == null) {
+                $scope.user.company = "Data not available";
+            }
+            $http.get($scope.user.repos_url)
+                .then(repositoryDetails, onError)
+            console.log('$scope.user', $scope.user);
+        };
+        var repositoryDetails = function(response) {
+            $scope.repositoryDetails = response.data;
+            $scope.error = "";
+            console.log('$scope.repositoryDetails', $scope.repositoryDetails);
+        }
+        var onError = function(reason) {
+        		$scope.user = false;
+            $scope.error = "Could not fetch the data";
+        }
+        $scope.search = function(userName) {
+            $http.get("https://api.github.com/users/" + userName)
+                .then(onUserComplete, onError);
+        }
+        $scope.filterByValue = 'language';
+        $scope.userName = "gsivaprabu";
+    };
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
---
+++ 01/05-Directives and Views/06-Directives/Readme.md
@@ -0,0 +1,32 @@
+# ng-directives concepts
+
+###List of Directives
+
+#### ng-app
+#### ng-bind
+#### ng-blur
+#### ng-change
+#### ng-class
+#### ng-click
+#### ng-copy
+#### ng-dbclick
+#### ng-disabled
+#### ng-focus
+#### ng-hide
+#### ng-href
+#### ng-if
+#### ng-include
+#### ng-init
+#### ng-keydown
+#### ng-keypress
+#### ng-keyup
+#### ng-model
+#### ng-mouseenter
+#### ng-mouseleave
+#### ng-mousemove
+#### ng-mouseover
+#### ng-paste
+#### ng-repeat
+#### ng-style
+#### ng-switch
+#### ng-transclude
