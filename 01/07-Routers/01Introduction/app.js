f1082a7322369c05ecc486725a920366807d178e
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-10-23 21:20:47 +0530

    Router concepts

---
+++ 01/07-Routers/01Introduction/app.js
@@ -0,0 +1,16 @@
+(function() {
+    var app = angular.module("githubViewer", ["ngRoute"]);
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "main.html",
+                controller: "mainController"
+            })
+            .when("/user/:username",{
+            		templateUrl : "user.html",
+            		controller:"userController"
+            })
+            .otherwise({ redirectTo: "/main" });
+    });
+}());
