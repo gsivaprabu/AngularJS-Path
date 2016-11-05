e97d8f413baabdf017a1700fd01b62da92034fd6
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-11-05 23:20:07 +0530

    Two way binding concepts

--- 02/2Controllers and Markup/11TwoWayBinding/Readme.md
+++ 02/2Controllers and Markup/11TwoWayBinding/Readme.md
@@ -15,3 +15,20 @@
 
 
 	<input type="text" ng-model="object.container.property">
+
+
+### QUIZ
+
+1. Two way binding will update on every key stroke ?
+
+Yes
+
+2. Which HTML elements works with two way binding ?
+
+- Input
+- Select
+- Textarea
+
+3. What will happen if you reference a property that doesn't exist in a ng-model directive ?
+
+The property will be created automatically.
---
+++ 02/demoApp/app/js/controllers/EditEventController.js
@@ -0,0 +1,15 @@
+'use strict';
+
+eventsApp.controller('EditEventController',
+    function EditEventController($scope) {
+        $scope.saveEvent = function(event) {
+            window.alert('event' + event.name + ' saved!');
+        }
+
+        $scope.cancelEvent = function() {
+            window.location = "/EventDetails.html";
+        }
+
+        // $scope.event.imageUrl = "https://s-media-cache-ak0.pinimg.com/736x/20/1c/e9/201ce94e7998a27257cdc2426ae7060c.jpg"
+    }
+);
