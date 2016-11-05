cacc1d42e9466d3d014013f2315aa3b3a7db1257
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-11-05 23:20:51 +0530

    Two way binding concepts Changes

--- 02/demoApp/app/js/controllers/EditEventController.js
+++ 02/demoApp/app/js/controllers/EditEventController.js
@@ -2,6 +2,8 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
+    		$scope.event = [];
+        $scope.event.imageUrl = "https://s-media-cache-ak0.pinimg.com/736x/20/1c/e9/201ce94e7998a27257cdc2426ae7060c.jpg"
         $scope.saveEvent = function(event) {
             window.alert('event' + event.name + ' saved!');
         }
@@ -9,7 +11,5 @@ eventsApp.controller('EditEventController',
         $scope.cancelEvent = function() {
             window.location = "/EventDetails.html";
         }
-
-        // $scope.event.imageUrl = "https://s-media-cache-ak0.pinimg.com/736x/20/1c/e9/201ce94e7998a27257cdc2426ae7060c.jpg"
     }
 );
