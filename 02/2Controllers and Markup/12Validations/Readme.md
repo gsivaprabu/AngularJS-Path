62fd852b8fe0a79495b23c32886b39a3df966182
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-11-06 09:41:17 +0530

    Angularjs validations

---
+++ 02/2Controllers and Markup/12Validations/Readme.md
@@ -0,0 +1,39 @@
+# Validation
+
+Important types:-
+
+* Required
+* ng-pattern
+* Form Properties
+* CSS Classes
+
+
+### Notable Points in validation concept
+
+- required
+- ng-pattern
+
+Written style for invalid values
+
+    input.ng-invalid.ng-dirty {
+        background-color: gold;
+    }
+
+- ng-disabled="newEventForm.$invalid"
+
+
+
+
+## QUIZ
+
+1. In form validation, What is the opposite property to dirty ?
+
+Pristine
+
+2. Which directive lets you create a regex for validation ?
+
+ng-pattern
+
+3. What property does a form require in order to check validity ?
+
+a name
--- 02/demoApp/app/js/controllers/EditEventController.js
+++ 02/demoApp/app/js/controllers/EditEventController.js
@@ -2,11 +2,14 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-    		$scope.event = [];
+        $scope.event = [];
         $scope.event.imageUrl = "https://s-media-cache-ak0.pinimg.com/736x/20/1c/e9/201ce94e7998a27257cdc2426ae7060c.jpg"
-        $scope.saveEvent = function(event) {
-            window.alert('event' + event.name + ' saved!');
-        }
+        $scope.saveEvent = function(event, newEventForm) {
+            console.log('newEventForm', newEventForm);
+            if (newEventForm.$valid) {
+                window.alert('Event ' + event.name + ' saved!');
+            }
+        };
 
         $scope.cancelEvent = function() {
             window.location = "/EventDetails.html";
