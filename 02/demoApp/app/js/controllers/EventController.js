3fba064cae1fdfb5b575720085719293e1ccbd92
Sivaprabu Ganesan <naveenasivaprabu@team.ginfoclique.com>
2016-11-09 21:20:50 +0530

    using Angular http services

--- 02/demoApp/app/js/controllers/EventController.js
+++ 02/demoApp/app/js/controllers/EventController.js
@@ -1,15 +1,17 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope,eventData) {
-    		$scope.boolValue = true;
-    		$scope.myStyle ={color:'red'};
-    		$scope.myClass = 'blue';
+    function EventController($scope, eventData) {
+        $scope.boolValue = true;
+        $scope.myStyle = { color: 'red' };
+        $scope.myClass = 'blue';
         $scope.snippet = '<span style="color:red">h1 there</span>';
         $scope.buttonDisabled = false;
         $scope.sortOrder = "name";
         $scope.myHTML = 'I am an <code>HTML</code>string with ' + '<a href="#">links!</a> and other <em>stuff</em>';
-        $scope.event = eventData.event;
+        eventData.getEvent(function(event) {
+            $scope.event = event
+        });
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
