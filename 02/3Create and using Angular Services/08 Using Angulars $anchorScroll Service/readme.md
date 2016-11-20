934847a7367416c9c8c360492d29d5ccecb999f5
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-11-20 22:03:39 +0530

    Using Angular's  Service

---
+++ 02/3Create and using Angular Services/08 Using Angulars $anchorScroll Service/readme.md
@@ -0,0 +1,5 @@
+#Using Angular's $anchorScroll Service
+
+- $anchorScroll navigates to the particular diva element
+- scrolls to an element with that id
+-
--- 02/demoApp/app/js/controllers/EventController.js
+++ 02/demoApp/app/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData, $log) {
+    function EventController($scope, eventData, $log, $anchorScroll) {
         $scope.sortorder = 'name';
         eventData.getEvent()
             .$promise
@@ -21,6 +21,10 @@ eventsApp.controller('EventController',
 
         $scope.downVoteSession = function(session) {
             session.upVoteCount--;
+        };
+
+        $scope.scrollToSession = function() {
+            $anchorScroll();
         }
     }
 );
