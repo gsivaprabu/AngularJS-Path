323156c7cdfd14f686600a0185ae76192c2e3f2b
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2016-11-21 00:02:21 +0530

    Using Angulars parse Service

---
+++ 02/3 Create and using Angular Services/12 Using Angular's $parse Service/readme.md
@@ -0,0 +1,6 @@
+#Using Angular's $parse Service
+- Parse  service is similar to compile service
+- Used to evaluate an expression
+- context1 is overriding context2
+	- getter
+	- setter
--- 02/demoApp/app/js/controllers/CompileSampleController.js
+++ 02/demoApp/app/js/controllers/CompileSampleController.js
@@ -1,6 +1,32 @@
 'use strict';
 eventsApp.controller('CompileSampleController',
-    function CompileSampleController($scope, $compile) {
+    function CompileSampleController($scope, $compile, $parse) {
+
+        var fn = $parse('1+2');
+        console.log('fn', fn);
+        console.log('fn', fn());
+        console.log('');
+
+        var getter = $parse('event.name');
+
+        var context1 = { event: { name: 'AngularJS Boot Camp' } };
+        var context2 = { event: { name: 'Code Camp' } };
+        console.log('context1', context1);
+        console.log('context2', context2);
+        console.log('');
+
+        console.log('context1', getter(context1));
+        console.log('context2', getter(context2));
+        console.log('');
+        console.log(getter(context2, context1));
+
+        var setter = getter.assign;
+        setter(context2, 'Code Retreat');
+        console.log('');
+
+        console.log(context2.event.name);
+
+
         $scope.appendDivToElement = function(markup) {
             return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
         }
