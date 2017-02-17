5bf91dec76a9fcaf386ae8c2ae1c6233389df793
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-02-17 07:03:56 +0530

    Creating Your First Angular Directive

--- 02/demoApp/app/js/app.js
+++ 02/demoApp/app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
-    .config(function($routeProvider) {
+    .config(function($routeProvider, $locationProvider) {
         $routeProvider.when('/newEvent', {
             templateUrl: 'templates/NewEvent.html',
             controller: 'EditEventController'
@@ -11,7 +11,6 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
             controller: 'EventListController'
         });
         $routeProvider.when('/event/:eventId', {
-            foo: 'bar',
             templateUrl: 'templates/EventDetails.html',
             controller: 'EventController',
             resolve: {
@@ -20,4 +19,12 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
                 }
             }
         });
+        $routeProvider.when('/sampleDirective', {
+            templateUrl: 'templates/SampleDirective.html',
+            controller: 'SampleDirectiveController'
+        })
+        $routeProvider.otherwise({ redirectTo: '/events' });
+
+        $locationProvider.html5Mode(true);
+
     });
