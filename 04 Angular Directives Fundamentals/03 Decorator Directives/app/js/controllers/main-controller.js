aa3c6d39cffb4e2214dd6c81b39b62b1baa160bf
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-03-03 08:16:46 +0530

    Decorator Directives - Demo Adding Pause Notification to HTML5 Videos

--- 04 Angular Directives Fundamentals/03 Decorator Directives/app/js/controllers/main-controller.js
+++ 04 Angular Directives Fundamentals/03 Decorator Directives/app/js/controllers/main-controller.js
@@ -1,29 +1,8 @@
 app.controller('mainCtrl', function($scope) {
-    $scope.user1 = {
-            name: 'Pranav Sivaprabu',
-            address: {
-                street: 'PO Box 123',
-                city: 'Secret Rebel Base',
-                planet: 'Yavin 4'
-            },
-            friends: [
-                'Sivaprabu',
-                'Naveena',
-                'Karthick'
-            ]
-        },
-        $scope.user2 = {
-            name: 'Luke Skywalker',
-            address: {
-                street: 'PO Box 123',
-                city: 'Secret Rebel Base',
-                planet: 'Yavin 4'
-            },
-            friends: [
-                'Han',
-                'Leia',
-                'Chewbacca'
-            ]
-        }
-    console.log('Parent Scope', $scope);
+    $scope.messages = [];
+    $scope.handlePause = function(e) {
+        console.log(e);
+        $scope.messages.push({ text: 'paused!' });
+        console.log('paused!');
+    }
 });
---
+++ 04 Angular Directives Fundamentals/03 Decorator Directives/app/js/directives/event-pause-directive.js
@@ -0,0 +1,13 @@
+app.directive('eventPause', function($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, el, attrs) {
+      var fn = $parse(attrs['eventPause']);
+      el.on('pause', function(event) {
+        scope.$apply(function() {
+          fn(scope, {evt: event})
+        })
+      })
+    }
+  }
+})
