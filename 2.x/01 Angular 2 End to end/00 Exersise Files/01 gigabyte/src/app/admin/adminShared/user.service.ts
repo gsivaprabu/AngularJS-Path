d4e852dfad04eeb1cb72550ee34987d693bda950
Sivaprabu Ganesan <gsivaprabu@gmail.com>
2017-08-04 15:19:43 +0530

    Changes

--- 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/adminShared/user.service.ts
+++ 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/adminShared/user.service.ts
@@ -6,10 +6,21 @@ import {
   RouterStateSnapshot
 } from "@angular/router";
 
+import * as  firebase from 'firebase';
+
 @Injectable()
 export class UserService implements CanActivate {
   userLoggedIn: boolean = false;
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDThsQmdNX_2rIADIttnanzlIjM7UegC18",
+      authDomain: "gigabytegames-6f4af.firebaseapp.com",
+      databaseURL: "https://gigabytegames-6f4af.firebaseio.com",
+      projectId: "gigabytegames-6f4af",
+      storageBucket: "gigabytegames-6f4af.appspot.com",
+      messagingSenderId: "793776193913"
+    });
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
