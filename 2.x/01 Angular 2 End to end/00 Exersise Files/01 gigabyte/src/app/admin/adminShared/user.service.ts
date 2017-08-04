38bf6c87751aae6b9f5e1106d26aab6b49d4e9f5
Naveena Sivaprabu <naveenasivaprabu@gmail.com>
2017-08-04 22:18:37 +0530

    login Changes

--- 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/adminShared/user.service.ts
+++ 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/adminShared/user.service.ts
@@ -6,11 +6,13 @@ import {
   RouterStateSnapshot
 } from "@angular/router";
 
-import * as  firebase from 'firebase';
+import * as firebase from "firebase";
 
 @Injectable()
 export class UserService implements CanActivate {
   userLoggedIn: boolean = false;
+  loggedInUser: string;
+  authUser: any;
   constructor(private router: Router) {
     firebase.initializeApp({
       apiKey: "AIzaSyDThsQmdNX_2rIADIttnanzlIjM7UegC18",
@@ -37,5 +39,43 @@ export class UserService implements CanActivate {
 
     this.router.navigate(["/admin/login"]);
     return false;
+  }
+
+  register(email: string, password: string) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function(error) {
+        alert(`${error.message} Please Try Again!`);
+      });
+  }
+  verifyUser() {
+    this.authUser = firebase.auth().currentUser;
+    if (this.authUser) {
+      alert(`Welcome ${this.authUser.email}`);
+      this.loggedInUser = this.authUser.email;
+      this.userLoggedIn = true;
+      this.router.navigate(["/admin"]);
+    }
+  }
+
+  login(loginEmail: string, loginPassword: string) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(loginEmail, loginPassword)
+      .catch(function(error) {
+        alert(`${error.message} Unable to login. Try again!`);
+      });
+  }
+  logout() {
+    this.userLoggedIn = false;
+    firebase.auth().signOut().then(
+      function() {
+        alert(`Logged Out!`);
+      },
+      function(error) {
+        alert(`${error.message} Unable to logout. Try again!`);
+      }
+    );
   }
 }
