ab337d7676f2ca7bd76c6622f8232eb3af3176ec
Naveena Sivaprabu <naveenasivaprabu@gmail.com>
2017-09-06 09:49:09 +0530

    Angular Getting Started - 02 Introduction - 01 Introduction

--- 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/blogAdd/blog-addComponent.ts
+++ 2.x/01 Angular 2 End to end/00 Exersise Files/01 gigabyte/src/app/admin/blogAdd/blog-addComponent.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import  {BlogAdminService} from '../adminShared/blog-admin.services';
-import {Blog} from '../adminShared/blog';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { BlogAdminService } from "../adminShared/blog-admin.services";
+import { Blog } from "../adminShared/blog";
 
 @Component({
-    selector:'add-menu',
-    templateUrl:'./blog-addComponent.html',
-
+  selector: "add-menu",
+  templateUrl: "./blog-addComponent.html"
 })
+export class BlogAddComponent {
+  imgTitle: string;
+  imageSRC: string;
+  postTitle: string;
+  content: string;
+  post: Blog;
+}
---
+++ 2.x/03 Angular Getting Started/01 Introduction/01 Introduction/README.md
@@ -0,0 +1,23 @@
+# Angular Getting Started - 02 Introduction - 01 Introduction
+
+- A JavaScript framework
+- For building client-side applications
+- Using HTML, CSS and JavaScript
+
+### Why Angular?
+- Expressive HTML
+- Powerful Data Binding
+- Modular By Design
+- Built-in Back-End Integration
+
+## Why a New Angular?
+- Built for Speed
+- Modern
+- Simplified API
+- Enhances Productivity
+
+## Module Overview
+- Anatomy of an Angular Application
+- Getting the Most from This Course
+- Sample Application
+- Course Outline
